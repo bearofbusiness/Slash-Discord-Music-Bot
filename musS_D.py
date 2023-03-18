154c7461a6614b5e8ab816e53e785da9edd81196
Bearofbusines <61521224+Bearofbusines@users.noreply.github.com>
2023-03-18 12:22:52 -0400

    updated library

---
+++ musS_D.py
@@ -0,0 +1,146 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+import asyncio
+import functools
+from io import StringIO
+import itertools
+import math
+import random
+from datetime import datetime
+import os
+import sys
+from dotenv import load_dotenv
+
+import json
+from yt_dlp import YoutubeDL
+
+
+load_dotenv()  # getting the key from the .env file
+key = os.environ.get('key')
+
+
+def pront(content, lvl="DEBUG", end="\n"):
+    colors = {
+        "LOG": "",
+        "DEBUG": "\033[1;95m",
+        "OKBLUE": "\033[94m",
+        "OKCYAN": "\033[96m",
+        "OKGREEN": "\033[92m",
+        "WARNING": "\033[93m",
+        "ERROR": "\033[91m",
+        "NONE": "\033[0m"
+    }
+    # if type(content) != str and type(content) != int and type(content) != float:
+    #    content = sep.join(content)
+    print(colors[lvl] + "{" + datetime.now().strftime("%x %X") +
+          "} " + lvl + ": " + str(content) + colors["NONE"], end=end)  # sep.join(list())
+
+
+# Returns a random hex code
+async def getRandomHex(seed):
+    random.seed(seed)
+    return random.randint(0, 16777215)
+
+
+# Creates a standard Embed object
+async def getEmbed(ctx, title='', content='', footer='', color=''):
+    if color == '':
+        color = await getRandomHex(ctx.author.id)
+    embed = discord.Embed(
+        title=title,
+        description=content,
+        color=color
+    )
+    embed.set_author(name=ctx.author.display_name,
+                     icon_url=ctx.author.display_avatar.url)
+    # TODO Hide the footer until i find out what to do with it
+    # embed.set_footer(footer=footer)
+    return embed
+
+
+# Creates and sends an Embed message
+async def send(ctx, title='', content='', footer='', color=''):
+    embed = await getEmbed(ctx, title, content, footer)
+    await ctx.send(embed=embed)
+
+
+class Bot(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents(value=3467840))
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        pront("Bot is ready", lvl="OKGREEN")
+
+
+bot = Bot()
+tree = app_commands.CommandTree(bot)
+
+
+@tree.command(name="ping", description="The ping command")
+async def _ping(interaction: discord.Interaction):
+    pront(dir(interaction), end="\n\n")
+    pront(dir(interaction.channel), end="\n\n")
+    pront(dir(interaction.client), end="\n\n")
+    pront(dir(interaction.user), end="\n\n")
+    # await send(interaction, title='Pong!', content=':ping_pong:')
+    await interaction.response.send_message('Pong!', ephemeral=True)
+
+
+bot.run(key)  # make sure you set your intents in the portal and here on line 10
+
+
+'''
+@bot.command(aliases=['eval'])
+# @tree.command(name="commandname", description="My first application Command")
+# @bot.tree.command(name="eval", description="The eval command",)
+@commands.is_owner()
+async def _eval(ctx: discord.Interaction, comand=None):
+    # if (ctx.author.id == {idhere}):#for when you are not owner but want to make it so only you can use it
+    # pront("LOG", comand)
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    comand.rstrip("`")
+    comand.lstrip("`")
+    comand.lstrip("python")
+    try:
+        print(eval(comand))
+    except Exception as e:
+        pront(e, "ERROR")
+    sys.stdout = old_stdout
+    # pront("LOG", mystdout.getvalue())
+    print(mystdout.getvalue())
+    await send(ctx, title='Command Sent:', content='in:\n```' + comand + '```' + '\n\nout:```ansi\n' + str(mystdout.getvalue()) + '```')
+#    else:#sends no perms if has none
+#        await send(ctx, title='You Do Not Have Perms')
+
+
+@bot.command(aliases=['exec'])
+@commands.is_owner()
+async def _exec(ctx, *, comand=None):
+    # if (ctx.author.id == {idhere}):#for when you are not owner 369999044023549962
+    # pront("LOG", comand)
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    comand = comand.rstrip("`")
+    comand = comand.lstrip("`")
+    comand = comand.lstrip("python")
+    # pront(comand)
+
+    try:
+        exec(comand)
+    except Exception as e:
+        pront(e, "ERROR")
+    sys.stdout = old_stdout
+    # pront("LOG", mystdout.getvalue())
+    print(mystdout.getvalue())
+    await send(ctx, title='Command Sent:', content='in:\n```' + comand + '```' + '\n\nout:```ansi\n' + str(mystdout.getvalue()) + '```')
+#    else:#sends no perms if has none
+#        await send(ctx, title='You Do Not Have Perms')
+'''
--- musS_I.py
+++ musS_I.py
@@ -1,3 +1,8 @@
+
+#!!!DEPRECATED!!!#
+#!!!DEPRECATED!!!#
+# do not use this file, it is deprecated and will not work#
+
 import interactions
 import discord
 from discord.ext import commands
@@ -33,11 +38,12 @@ TODO:
 load_dotenv()
 key = os.environ.get('key')
 
-
+dpy = commands.Bot(command_prefix="/")
 bot = interactions.Client(token=key)
+state = interactions.api.models.gw.VoiceState()
 
 
-def pront(content, lvl="DEBUG"):
+def pront(*content, lvl="DEBUG"):
     colors = {
         "LOG": "",
         "DEBUG": "\033[1;95m",
@@ -87,7 +93,7 @@ async def test_command(ctx: interactions.CommandContext):
     await send(ctx, title="Hello World!", footer="MaBalls")
 
 
-@commands.is_owner()
+# @commands.is_owner() !Doesn't work!
 @bot.command(
     name="execute",
     description="This is the exec command only can be used be owner for obvious reasons",
@@ -123,4 +129,32 @@ async def execute(ctx, input: str):
         await send(ctx, title='You Do Not Have Perms', footer='MABALLS')
 
 
-bot.start()
+@bot.command(
+    name="join",
+    description="This is the join/leave vc",
+)
+async def join(ctx):
+    print(dir(ctx.member), ctx.member.voice_state, ctx.member.user.id)
+    # print(dir(ctx.guild), dir(ctx.guild.voice_states))
+    # if ctx.guild.voice_client is not None:
+    #     print('already in vc, leaving.')
+    #     await ctx.voice_client.disconnect()
+    #     return
+    # authorVoiceState = ctx.author.voice
+    # if authorVoiceState is None or ctx.author.voice.channel is None:
+    #     send(ctx, title='You are not in a voice channel', footer='MABALLS')
+    #     return
+    # voice_channel = ctx.author.voice.channel
+
+
+@dpy.command()
+async def join(ctx):
+    ctx.send('Joining vc')
+
+loop = asyncio.get_event_loop()
+
+task2 = loop.create_task(dpy.start(token=key, bot=True))
+task1 = loop.create_task(bot.start())
+
+gathered = asyncio.gather(task1, task2, loop=loop)
+loop.run_until_complete(gathered)
