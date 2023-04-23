9d53b7259c9b90163355d9ceb230297a2264e9ff
Technonerd5 <noahnmj@gmail.com>
2023-04-23 14:58:04 -0400

    increased yt_dlp verbosity

--- YTDLInterface.py
+++ YTDLInterface.py
@@ -18,8 +18,8 @@ class YTDLInterface:
         'nocheckcertificate': True,
         'ignoreerrors': False,
         'logtostderr': False,
-        'quiet': True,
-        'no_warnings': True,
+        'quiet': False,
+        'no_warnings': False,
         'default_search': 'auto',
         'source_address': '0.0.0.0',
     }
