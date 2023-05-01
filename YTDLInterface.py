f499aa2a3357c0c8bd073745a5d357f729ef1a2f
Technonerd5 <noahnmj@gmail.com>
2023-05-01 08:54:48 -0400

    Condensed YTDLInterface to call yt_dlp from one method

--- YTDLInterface.py
+++ YTDLInterface.py
@@ -44,44 +44,30 @@ class YTDLInterface:
     # Rapidy retrieves shell information surrounding a URL
     @staticmethod
     async def scrape_link(link: str = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ') -> dict:
-        # Define asyncio loop
-        loop = asyncio.get_event_loop()
-
-        with yt_dlp.YoutubeDL(YTDLInterface.scrape_options) as ytdlp:
-            # Define ytdlp command within a partial to be able to run it within run_in_executor
-            partial = functools.partial(
-                ytdlp.extract_info, link, download=False)
-            query_result = await loop.run_in_executor(None, partial)
-        
-        return query_result
+        return await YTDLInterface.__call_dlp(YTDLInterface.scrape_options, link)
 
     # Only called to automatically resolve searches input into scrape_link
     # Pulls information from a yt-dlp accepted URL and returns a Dict containing that information
     @staticmethod
     async def query_link(link: str = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ') -> dict:
+        await YTDLInterface.__call_dlp(YTDLInterface.retrieve_options, link)
+
+
+    # Searches for a provided string
+    @staticmethod
+    async def scrape_search(query: str) -> dict:
+        await YTDLInterface.__call_dlp(YTDLInterface.scrape_options, f'ytsearch5:{query}')
+
+    # Private method to condense all the others
+    @staticmethod
+    async def __call_dlp(options: dict, link: str):
         # Define asyncio loop
         loop = asyncio.get_event_loop()
 
-        with yt_dlp.YoutubeDL(YTDLInterface.retrieve_options) as ytdlp:
+        with yt_dlp.YoutubeDL(options) as ytdlp:
             # Define ytdlp command within a partial to be able to run it within run_in_executor
             partial = functools.partial(
                 ytdlp.extract_info, link, download=False)
             query_result = await loop.run_in_executor(None, partial)
 
         return query_result
-
-
-    # Searches for a provided string
-    @staticmethod
-    async def scrape_search(query: str) -> dict:
-        # Define asyncio loop
-        loop = asyncio.get_event_loop()
-
-        with yt_dlp.YoutubeDL(YTDLInterface.scrape_options) as ytdlp:
-            # Define ytdlp command within a partial to be able to run it within run_in_executor
-            partial = functools.partial(
-                ytdlp.extract_info, f'ytsearch5:{query}', download=False)
-            query_result = await loop.run_in_executor(None, partial)
-
-        return query_result
-
