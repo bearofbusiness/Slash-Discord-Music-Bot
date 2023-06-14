1d3755448f3d8bd5c119f1e0d8fdbdc28124a9f7
Sm1mg <noahnmj@gmail.com>
2023-06-14 00:39:44 -0400

    fixed poorly ported code in get_embed

--- Utils.py
+++ Utils.py
@@ -61,7 +61,7 @@ def get_embed(interaction, title='', content='', url=None, color='', progress: b
     # If the calling method wants the progress bar
     if progress:
         player = Servers.get_player(interaction.guild_id)
-        if player is not None and player.queue.get():
+        if player and player.song:
             footer_message = f'{"🔂 " if player.looping else ""}{"🔁 " if player.queue_looping else ""}{"♾ " if player.true_looping else ""}\n{get_progress_bar(player.song)}'
 
             embed.set_footer(text=footer_message,
