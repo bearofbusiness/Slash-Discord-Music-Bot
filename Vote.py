4aeff57c3e90b0726d0119dc634f1353ce6499a4
Sm1mg <noahnmj@gmail.com>
2023-07-29 02:17:12 -0400

    Wrote pydocs for Vote.py

--- Vote.py
+++ Vote.py
@@ -2,6 +2,23 @@ from discord import Member
 
 
 class Vote:
+    """
+    A class for keeping track of users who have voted to do x
+
+    Attributes
+    ----------
+    initiator : discord.Member
+        The Member who initiated the Vote.
+    voters :  list[discord.Member]
+        A list of all members who have voted, from first to last.    
+
+    Methods
+    -------
+    add(member: discord.Member)
+        Adds a Member to the list of voters.
+    get()
+        Returns the list of voters
+    """
     def __init__(self, initiator: Member):
         self.initiator = initiator
         self.voters = [initiator]
@@ -10,8 +27,20 @@ class Vote:
         return len(self.voters)
 
     def add(self, member: Member) -> None:
+        """
+        Parameters
+        ----------
+        member : discord.Member
+            The Member to be added to the list of voters.
+        """
         self.voters.append(member)
         return
 
     def get(self) -> list[Member]:
+        """
+        Returns
+        -------
+        list[discord.Member]:
+            A list of all members who have voted, from first to last.
+        """
         return self.voters
