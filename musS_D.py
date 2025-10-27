f6e13702f1bf03dce097776b1a77f0501923faec
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-10-26 21:16:31 -0600

    set update command to be unloaded by default

--- musS_D.py
+++ musS_D.py
@@ -64,7 +64,7 @@ class Bot(commands.Bot):  # initiates the bots intents and on_ready event
         await self.load_extension("cogs.QueueManagement")
         await self.load_extension("cogs.PlaybackManagement")
         await self.load_extension("cogs.PlayerManagement")
-        await self.load_extension("cogs.Update")
+        # await self.load_extension("cogs.Update") # uncomment to enable self updating
         #await self.load_extension("cogs.DebugCog")
         Utils.pront("Cogs loaded!")
 
